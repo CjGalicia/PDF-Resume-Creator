47069071a00c7f4ddf1cfcc3bdc30e89472f9641
CjGalicia <carljeremygalicia@gmail.com>
2022-07-08 21:24:33 +0800

    design of form1 done

---
+++ PDFresumeCreatorvb/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.CreatePDFbtn = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ResumeLabel = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CreatePDFbtn
+        '
+        Me.CreatePDFbtn.Font = New System.Drawing.Font("Bahnschrift Condensed", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CreatePDFbtn.Location = New System.Drawing.Point(78, 140)
+        Me.CreatePDFbtn.Name = "CreatePDFbtn"
+        Me.CreatePDFbtn.Size = New System.Drawing.Size(132, 43)
+        Me.CreatePDFbtn.TabIndex = 0
+        Me.CreatePDFbtn.Text = "Generate Resume"
+        Me.CreatePDFbtn.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(78, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(132, 87)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'ResumeLabel
+        '
+        Me.ResumeLabel.AutoSize = True
+        Me.ResumeLabel.Font = New System.Drawing.Font("Bahnschrift Condensed", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ResumeLabel.Location = New System.Drawing.Point(32, 102)
+        Me.ResumeLabel.Name = "ResumeLabel"
+        Me.ResumeLabel.Size = New System.Drawing.Size(225, 19)
+        Me.ResumeLabel.TabIndex = 2
+        Me.ResumeLabel.Text = "Create Better Resume. To get Better Jobs!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(280, 217)
+        Me.Controls.Add(Me.ResumeLabel)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CreatePDFbtn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Form1"
+        Me.Text = "Resume Creator"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CreatePDFbtn As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ResumeLabel As Label
+End Class
